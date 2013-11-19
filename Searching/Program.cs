b4a0cd86bd0894e5c1f722882b29c0fa0f70ad6e
arkosp <apiznal@gmail.com>
2013-11-19 19:17:08 +0100

    poczatek pracy nad grafami

--- Searching/Program.cs
+++ Searching/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace Searching
 {
@@ -10,10 +11,10 @@ namespace Searching
         static void Main(string[] args)
         {
             var numbers = new[] { 1, 3, 5, 6, 8, 12, 20, 33, 50, 82, 90, 120, 130, 178, 456, 789, 999, 1020 };
-            
+
             Console.WriteLine(BinarySearch.Recursve(numbers, 1020));
             Console.WriteLine(BinarySearch.Iterative(numbers, 1020));
-            
+
             Console.ReadLine();
         }
     }
